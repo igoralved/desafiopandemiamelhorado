79b78a9cc71fe81101ecd8abefac4e9150bbb0db
igoralved <87991535+igoralved@users.noreply.github.com>
2022-04-08 15:13:54 -0300

    Add files via upload

--- src/test/igorgroup/desafiopandemia/controller/EtapaControllerTest.java
+++ src/test/igorgroup/desafiopandemia/controller/EtapaControllerTest.java
@@ -1,10 +1,13 @@
 package igorgroup.desafiopandemia.controller;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.BDDMockito;
@@ -20,6 +23,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import igorgroup.desafiopandemia.model.Etapa;
 import igorgroup.desafiopandemia.repository.EtapaRepository;
@@ -38,6 +42,8 @@ public class EtapaControllerTest {
 	@MockBean
 	private EtapaRepository er;
 	
+	
+	
 	@Autowired
 	private MockMvc mockMvc;
 	
@@ -50,6 +56,9 @@ public class EtapaControllerTest {
 		}
 	}
 	
+
+	
+	
 	@Test
 	public void testaLista() {
 		restTemplate = restTemplate.withBasicAuth("1", "1");
@@ -59,18 +68,19 @@ public class EtapaControllerTest {
 	
 	@Test
 	public void testaListaComMock() {
+		
 		//etapa 1
-		Etapa e1 = new Etapa();
-		e1.setId(1L);
-		e1.setNumero(1);
-		e1.setDescricao("Bem bom");
-		
-		//etapa 2
-		
-		Etapa e2 = new Etapa();
-		e2.setId(2L);
-		e2.setNumero(2);
-		e2.setDescricao("Tudo certo");
+				Etapa e1 = new Etapa();
+				e1.setId(1L);
+				e1.setNumero(1);
+				e1.setDescricao("Bem bom");
+				
+				//etapa 2
+				
+				Etapa e2 = new Etapa();
+				e2.setId(2L);
+				e2.setNumero(2);
+				e2.setDescricao("Tudo certo");
 		
 		List<Etapa> lista = new ArrayList<>();
 		lista.add(e1);
@@ -85,7 +95,7 @@ public class EtapaControllerTest {
 	}
 	
 	@Test
-	public void testaEtapaUnicaComMock() {
+	public void testaDetalheComMock() {
 		//etapa 1
 		Etapa e = new Etapa();
 		e.setId(1L);
@@ -101,5 +111,89 @@ public class EtapaControllerTest {
 		
 		Assertions.assertThat(result.getStatusCodeValue()).isEqualTo(200);
 	}
+	
+	@Test
+	public void testaDetalheComMockFracasso() {
+		//etapa 1
+		Etapa e = new Etapa();
+		e.setId(1L);
+		e.setNumero(1);
+		e.setDescricao("Bem bom");
+			
+		Optional<Etapa> eo = Optional.of(e);
+				
+		BDDMockito.when(er.findById(e.getId())).thenReturn(eo);
+		System.out.println("ID " + e.getId());
+				
+		ResponseEntity<Etapa> result = restTemplate.getForEntity("/etapas/detalhar/{id}", Etapa.class, 2L);
+				
+		Assertions.assertThat(result.getStatusCodeValue()).isEqualTo(404);
+	}
+	
+	@Test
+	public void testaCadastro() {
+		Etapa e = new Etapa();
+		e.setId(1L);
+		e.setNumero(1);
+		e.setDescricao("É para adultos");
+		
+		Optional<Etapa> eo = Optional.of(e);
+		
+		BDDMockito.when(er.save(e)).thenReturn(e);
+		
+		ResponseEntity<Etapa> result = restTemplate.postForEntity("/etapas/cadastrar", e, Etapa.class);
+	
+		Assertions.assertThat(result.getStatusCodeValue()).isEqualTo(201);
+	}
+	
+	@Test
+	public void testaAtualizacao() {
+		Etapa e = new Etapa();
+		e.setId(1L);
+		e.setNumero(1);
+		e.setDescricao("Para acertos");
+		
+		Optional<Etapa> eo = Optional.of(e);
+		
+		BDDMockito.when(er.findById(e.getId())).thenReturn(eo);
+	
+		Assertions.assertThat(eo).isPresent();
+		
+	}
+	
+	@Test
+	public void testaAtualizacaoComFracasso() {
+		
+		Optional<Etapa> eo = er.findById(200L);
+		
+		BDDMockito.when(er.findById(200L)).thenReturn(eo);
+	
+		Assertions.assertThat(eo).isEmpty();
+		
+	}
+	
+	@Test
+	public void testaRemocao() {
+		Etapa e = new Etapa();
+		e.setId(200L);
+		e.setNumero(1);
+		e.setDescricao("Para acertos");
+		
+		//System.out.println("Remocao antes " + er.findById(e.getId()).get().getId());
+		
+		er.save(e);
+		
+		BDDMockito.when(er.findById(e.getId())).thenReturn(Optional.of(e));
+		
+		Assertions.assertThat(er.findById(e.getId())).isPresent();
+		
+		er.deleteById(e.getId());
+		
+		BDDMockito.when(er.findById(e.getId())).thenReturn(Optional.empty());
+		
+		Assertions.assertThat(er.findById(e.getId())).isEmpty();
+		
+	}
+	
 	
 }

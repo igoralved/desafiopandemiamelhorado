7e42817a761070e710fff4af19437c94e5b3aea4
igoralved <87991535+igoralved@users.noreply.github.com>
2022-04-14 17:05:09 -0300

    Add files via upload

--- src/test/igorgroup/desafiopandemia/controller/TesteControllerTest.java
+++ src/test/igorgroup/desafiopandemia/controller/TesteControllerTest.java
@@ -1,6 +1,12 @@
 package igorgroup.desafiopandemia.controller;
 
 import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,8 +25,13 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import igorgroup.desafiopandemia.model.Etapa;
 import igorgroup.desafiopandemia.model.Teste;
@@ -37,8 +48,11 @@ public class TesteControllerTest {
 	@LocalServerPort
 	private int port;
 	
-	@MockBean
+	@Autowired
 	private TesteRepository tr;
+	
+	@Autowired
+	private MockMvc mockMvc;
 	
 	@TestConfiguration
 	static class Config{
@@ -48,7 +62,159 @@ public class TesteControllerTest {
 		}
 	}
 	
+	@Test
+	public void testaLista() {
+		try {
+			System.out.println();
+			System.out.println();
+			System.out.println("GET");
+			System.out.println();
+			System.out.println();
+			
+			this.mockMvc.perform(
+					get("/testes/todos"))
+				.andDo(print()).andExpect(status().isOk());
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
+	@Test
+	public void buscarPorId() {
+		System.out.println();
+		System.out.println();
+		System.out.println("GET{id}");
+		System.out.println();
+		System.out.println();
+		
+		
+		Teste t1 = new Teste();
+		t1.setId(1L);
+		t1.setNumero(1);
+		t1.setResultado("Aprovado");
+		
+		this.tr.save(t1);
+		
+		try {
+			this.mockMvc.perform(get("/testes/detalhar/{id}", t1.getId()))
+			.andDo(print()).andExpect(status().isOk());
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	@Test
+	public void buscarPorIdInexistente() {
+		System.out.println();
+		System.out.println();
+		System.out.println("GET{id} FAILURE");
+		System.out.println();
+		System.out.println();
+		
+		try {
+			this.mockMvc.perform(get("/testes/detalhar/{id}",100L))
+			.andDo(print()).andExpect(status().isNotFound());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void cadastraTeste() {
+		System.out.println();
+		System.out.println();
+		System.out.println("GET{id} FAILURE");
+		System.out.println();
+		System.out.println();
+		
+		try {
+			Teste t1 = new Teste();
+			t1.setId(2L);
+			t1.setNumero(2);
+			t1.setResultado("Metade lá");
+			
+			ObjectMapper objectMapper = new ObjectMapper();
+			String json = objectMapper.writeValueAsString(t1);
+			
+			MvcResult mvcResult = this.mockMvc.perform(post("/testes/cadastrar")
+					.contentType(MediaType.APPLICATION_JSON).content(json)).andDo(print())
+					.andExpect(status().isCreated())
+					.andReturn();
+			
+			this.mockMvc.perform(get("/testes/detalhar/{id}",t1.getId()))
+			.andDo(print()).andExpect(status().isOk()).andReturn();
+			
+			assertEquals("application/json;charset=UTF-8", mvcResult.getResponse().getContentType());
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void atualizar() {
+		
+		Teste t1 = new Teste();
+		t1.setId(1L);
+		t1.setNumero(1);
+		t1.setResultado("Aprovado");
+		
+		this.tr.save(t1);
+		
+		Teste t2 = new Teste();
+		t2.setId(2L);
+		t2.setNumero(2);
+		t2.setResultado("Metade lá");
+		
+		System.out.println();
+		System.out.println();
+		System.out.println("PUT");
+		System.out.println();
+		System.out.println();
+		
+		try {
+			
+			ObjectMapper objectMapper = new ObjectMapper();
+			String json = objectMapper.writeValueAsString(t1);
+			
+			this.mockMvc.perform(put("/testes/atualizar/{id}",t1.getId())
+					.contentType(MediaType.APPLICATION_JSON)
+				.content(json))
+			.andDo(print()).andExpect(status().isOk());
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	@Test
+	public void deletar() {
+		System.out.println();
+		System.out.println();
+		System.out.println("DELETE");
+		System.out.println();
+		System.out.println();
+		
+		Teste t3 = new Teste();
+		t3.setId(3L);
+		t3.setNumero(3);
+		t3.setResultado("Graduado");
+		
+		this.tr.save(t3);
+		
+		try {
+			this.mockMvc.perform(delete("/testes/remover/{id}",t3.getId()))
+			.andDo(print()).andExpect(status().isOk());
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	/*
 	
 	@Test
 	public void test() {
@@ -184,7 +350,7 @@ public class TesteControllerTest {
 		Assertions.assertThat(tr.findById(t.getId())).isEmpty();
 		
 	}
-	
+	*/
 	
 
 }

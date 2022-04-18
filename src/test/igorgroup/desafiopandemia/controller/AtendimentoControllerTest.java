d0ab1eeea21a897b20904edae6c6efc404e60be9
igoralved <87991535+igoralved@users.noreply.github.com>
2022-04-18 17:07:43 -0300

    Add files via upload

--- src/test/igorgroup/desafiopandemia/controller/AtendimentoControllerTest.java
+++ src/test/igorgroup/desafiopandemia/controller/AtendimentoControllerTest.java
@@ -1,12 +1,27 @@
 package igorgroup.desafiopandemia.controller;
 
+import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
 import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,11 +32,20 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.web.servlet.server.Jsp;
 import org.springframework.context.annotation.Bean;
+import org.springframework.format.datetime.DateFormatter;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 import java.util.Optional;
+import java.util.TimeZone;
 
 import igorgroup.desafiopandemia.model.Atendimento;
 import igorgroup.desafiopandemia.repository.AtendimentoRepository;
@@ -31,6 +55,197 @@ import igorgroup.desafiopandemia.repository.AtendimentoRepository;
 @AutoConfigureMockMvc
 public class AtendimentoControllerTest {
 
+	@Autowired
+	private TestRestTemplate restTemplate;
+	
+	@LocalServerPort
+	private int port;
+	
+	@Autowired
+	private AtendimentoRepository ar;
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@TestConfiguration
+	static class Config{
+		@Bean
+		public RestTemplateBuilder restTemplateBuilder() {
+			return new RestTemplateBuilder().basicAuthentication("Igor","0000");
+		}
+	}
+	
+	@Test
+	public void testaLista() {
+		try {
+			
+			System.out.println();
+			System.out.println();
+			System.out.println("GET");
+			System.out.println();
+			System.out.println();
+			
+			this.mockMvc.perform(get("/atendimentos/listar"))
+			.andDo(print()).andExpect(status().isOk());
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void buscarPorId() {
+		System.out.println();
+		System.out.println();
+		System.out.println("GET/{id}");
+		System.out.println();
+		System.out.println();
+		
+		Atendimento a = new Atendimento();
+		a.setId(1L);
+		a.setDescricao("Atendendo agora");
+		a.setRelacionado_com_pandemia(true);
+		a.setSem_possibilidade_contagio(false);
+		a.setTempoAtendimento(12);
+		this.ar.save(a);
+		
+		try {
+			
+			this.mockMvc.perform(get("/atendimentos/detalhar/{id}", a.getId()))
+			.andDo(print())
+			.andExpect(status().isOk());
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	@Test
+	public void buscarPorIdInexistente() {
+		System.out.println();
+		System.out.println();
+		System.out.println("GET{id} FAILURE");
+		System.out.println();
+		System.out.println();
+		try {
+			
+			this.mockMvc.perform(get("/atendimentos/detalhar/{id}", 100L))
+			.andDo(print())
+			.andExpect(status().isNotFound());
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testaCadastro() {
+		System.out.println();
+		System.out.println();
+		System.out.println("POST");
+		System.out.println();
+		System.out.println();
+		
+		try {
+			
+			Atendimento a = new Atendimento();
+			a.setId(1L);
+			a.setDescricao("Atendendo até amanhã");
+			a.setRelacionado_com_pandemia(true);
+			a.setSem_possibilidade_contagio(false);
+			a.setTempoAtendimento(11);
+			
+			ObjectMapper objectMapper = new ObjectMapper();
+			String jsonString = "{\"descricao\":\"Atendendo agora\",\"data\":\"2022-04-18\",\"tempoAtendimento\":12,\"etapas\":[],\"testes\":[],\"relacionado_com_pandemia\":true,\"sem_possibilidade_contagio\":false,\"id\":1}";
+			
+			MvcResult mvcResult = this.mockMvc.perform(
+					post("/atendimentos/cadastrar")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(jsonString)
+						)
+					.andDo(print())
+					.andExpect(status().isCreated())
+					.andReturn();
+			
+			this.mockMvc.perform(get("/atendimentos/detalhar/{id}", a.getId()))
+			.andDo(print())
+			.andExpect(status().isOk());
+			
+			assertEquals("application/json;charset=UTF-8",mvcResult.getResponse().getContentType());
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	@Test
+	public void atualizar() {
+		
+		
+		System.out.println();
+		System.out.println();
+		System.out.println("PUT");
+		System.out.println();
+		System.out.println();
+		
+		try {
+			
+			Atendimento a1 = new Atendimento();
+			a1.setId(1L);
+			a1.setDescricao("Atendendo até depois de amanhã");
+			a1.setRelacionado_com_pandemia(true);
+			a1.setSem_possibilidade_contagio(true);
+			a1.setTempoAtendimento(11);
+			
+			this.ar.save(a1);
+			
+			ObjectMapper objectMapper = new ObjectMapper();
+			String jsonString = "{\"descricao\":\"Atendendo até depois de amanhã\",\"data\":\"2021-04-18\",\"tempoAtendimento\":12,\"etapas\":[],\"testes\":[],\"relacionado_com_pandemia\":true,\"sem_possibilidade_contagio\":false,\"id\":1}";
+			this.mockMvc.perform(put("/atendimentos/atualizar/{id}",a1.getId())
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(jsonString)
+			)
+			.andDo(print()).andExpect(status().isOk());
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	@Test
+	public void deleta() {
+
+		System.out.println();
+		System.out.println();
+		System.out.println("DELETE");
+		System.out.println();
+		System.out.println();
+		
+		
+		try {
+			
+			Atendimento a1 = new Atendimento();
+			a1.setId(2L);
+			a1.setDescricao("Atendendo até depois de amanhã");
+			a1.setRelacionado_com_pandemia(false);
+			a1.setSem_possibilidade_contagio(false);
+			a1.setTempoAtendimento(13);
+			
+			this.ar.save(a1);
+			
+			this.mockMvc.perform(delete("/atendimentos/remover/{id}",a1.getId()))
+			.andDo(print()).andExpect(status().isOk());
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/*
 	@Autowired
 	private TestRestTemplate restTemplate;
 	
@@ -188,5 +403,5 @@ public class AtendimentoControllerTest {
 		
 		
 	}
-
+*/
 }
